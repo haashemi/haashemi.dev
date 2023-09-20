64832c584aa2a82cfc4c2f652dba819c0f9751c2
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-09-20 16:13:53 +0330

    feat(routes): add wip page load function

---
+++ src/routes/+page.ts
@@ -0,0 +1,33 @@
+export function load() {
+	return {
+		info: {
+			title: 'Junior Back-end Developer',
+			text: 'Junior back-end developer with 3+ years of experience, including 1+ years of professional experience using Golang. Proficient in developing and maintaining high-performance, stable, and reliable web applications. Also interested in front-end development using modern web frameworks like React and Svelte.',
+		},
+
+		projects: [
+			{
+				title: '',
+				image: '',
+				url: '',
+			},
+		],
+
+		skills: [
+			{ name: 'Golang', style: 'hover:shadow-[0_0_30px_3px_#00ADD8d9] hover:bg-[#00ADD8]' },
+			{ name: 'JS/TS', style: 'hover:text-black hover:shadow-[0_0_30px_3px_#F7DF1Ed9] hover:bg-[#F7DF1E]' },
+			{ name: 'PostgreSQL', style: 'hover:shadow-[0_0_30px_3px_#4169E1d9] hover:bg-[#4169E1]' },
+			{ name: 'SQLite', style: 'hover:shadow-[0_0_30px_3px_#003B57d9] hover:bg-[#003B57]' },
+			{ name: 'Badger', style: 'hover:shadow-[0_0_30px_3px_#E50695d9] hover:bg-[#E50695]' },
+			{ name: 'Redis', style: 'hover:shadow-[0_0_30px_3px_#DC382Dd9] hover:bg-[#DC382D]' },
+			{ name: 'Next.JS', style: 'hover:shadow-[0_0_30px_3px_#000000d9] hover:bg-[#000000]' },
+			{ name: 'React', style: 'hover:text-black hover:shadow-[0_0_30px_3px_#61DAFBd9] hover:bg-[#61DAFB]' },
+			{ name: 'Svelte', style: 'hover:shadow-[0_0_30px_3px_#FF3E00d9] hover:bg-[#FF3E00]' },
+			{ name: 'TailwindCSS', style: 'hover:shadow-[0_0_30px_3px_#06B6D4d9] hover:bg-[#06B6D4]' },
+			{ name: 'Git', style: 'hover:shadow-[0_0_30px_3px_#F05032d9] hover:bg-[#F05032]' },
+			{ name: 'Docker', style: 'hover:shadow-[0_0_30px_3px_#2496EDd9] hover:bg-[#2496ED]' },
+			{ name: 'OpenTelemetry', style: 'hover:shadow-[0_0_30px_3px_#000000d9] hover:bg-[#000000]' },
+			{ name: 'Nginx', style: 'hover:shadow-[0_0_30px_3px_#009639d9] hover:bg-[#009639]' },
+		],
+	};
+}
