3558cc473f101918c5d09e9177ffd5666420922a
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-09-21 19:42:30 +0330

    chore: do some cringy stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import Header from '$lib/Header.svelte';
 	import '../app.css';
+
+	console.log('%c ----------------', 'font-size: 30px;');
+	console.log('%c BRO HIRE ME NOW!', 'font-size: 50px; font-style: italic;');
+	console.log('%c contact@haashemi.dev', 'font-style: italic;');
+	console.log('%c https://github.com/haashemi', 'font-style: italic;');
+	console.log('%c ----------------', 'font-size: 30px;');
 </script>
 
 <svelte:head>
@@ -8,8 +14,8 @@
 	<meta name="description" content="Ali Hashemi's portfolio website." />
 </svelte:head>
 
-<Header />
-
 <main class="container mx-auto flex flex-col gap-4 p-3">
+	<Header />
+
 	<slot />
 </main>
