246f6628cd9354bac547c778251e477a23eb7251
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-09-21 19:43:26 +0330

    chore(routes/Home): am... redesign? maybe?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,40 +12,63 @@
 	<div class="absolute bottom-0 right-0 -z-10 h-16 w-1/4 bg-slate-800 blur-3xl" />
 </div>
 
-<div class="flex flex-col gap-4 md:flex-row">
+<div class="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
 	<div class="space-y-5 rounded-xl border-2 border-slate-700/40 p-5">
-		<h3 class="text-lg font-semibold italic">Projects</h3>
+		<div class="flex items-center justify-between">
+			<h3 class="text-lg font-semibold italic">Projects</h3>
+			<a href="/" class="text-sm text-slate-300">View More</a>
+		</div>
 
-		<div class="flex items-center justify-around gap-4">
-			<a
-				href="/"
-				class="flex aspect-square h-20 w-20 items-center justify-center rounded-2xl bg-gradient-to-br
-				from-red-600 to-orange-600 p-5 shadow-lg shadow-red-700
-				transition-all ease-in-out hover:-translate-y-1 hover:shadow-red-800 motion-reduce:transition-none md:h-24 md:w-24"
-			>
-				<img src="/projects/epic-center.png" alt="Epic-Center" />
+		<div class="flex flex-col gap-4">
+			<a href="/" class="group relative flex h-20 gap-5 rounded-xl bg-stone-600/20">
+				<div class="w-20 rounded-xl bg-gradient-to-br from-red-600 to-red-700 p-3">
+					<img src="/projects/epic-center.png" alt="Epic-Center" />
+				</div>
+
+				<div class="flex flex-col justify-around whitespace-nowrap py-1 pr-5 font-sans">
+					<h2 class="text-lg font-semibold italic">Epic Center</h2>
+					<h3 class="text-xs">FullStack Developer</h3>
+					<p class="text-xs text-zinc-500">Aug 2022 - Present</p>
+				</div>
+
+				<div class="absolute -z-10 h-20 w-20 rounded-xl bg-red-900 blur-md transition-all group-hover:bg-red-800 group-hover:blur-lg" />
 			</a>
-			<a
-				href="/"
-				class="flex aspect-square h-20 w-20 items-center justify-center rounded-2xl
-				bg-gradient-to-br from-blue-700 to-sky-600 p-5 shadow-lg shadow-blue-600
-				transition-all ease-in-out hover:-translate-y-1 hover:shadow-blue-700 motion-reduce:transition-none md:h-24 md:w-24"
-			>
-				<img src="/projects/zainox.png" alt="Zainox" />
+
+			<a href="/" class="group relative flex h-20 gap-5 rounded-xl bg-white/5">
+				<div class="w-20 rounded-xl bg-gradient-to-br from-blue-600 to-blue-700 p-3">
+					<img src="/projects/zainox.png" alt="Zainox" />
+				</div>
+
+				<div class="flex flex-col justify-around whitespace-nowrap py-1 pr-5 font-sans">
+					<h2 class="text-lg font-semibold italic">Zainox</h2>
+					<h3 class="text-xs">Back-End Developer</h3>
+					<p class="text-xs text-zinc-500">Aug 2022 - Present</p>
+				</div>
+
+				<div class="absolute -z-10 h-20 w-20 rounded-xl bg-blue-900 blur-md transition-all group-hover:bg-blue-800 group-hover:blur-lg" />
 			</a>
-			<a
-				href="/"
-				class="flex aspect-square h-20 w-20 items-center justify-center rounded-2xl
-				bg-gradient-to-br from-purple-700 to-violet-600 p-5 shadow-lg shadow-violet-600
-				transition-all ease-in-out hover:-translate-y-1 hover:shadow-violet-700 motion-reduce:transition-none md:h-24 md:w-24"
-			>
-				<img src="/projects/llamanite.png" alt="Zainox" />
+
+			<a href="/" class="group relative flex h-20 gap-5 rounded-xl bg-white/5">
+				<div class="w-20 rounded-xl bg-gradient-to-br from-violet-600 to-violet-700 p-3">
+					<img src="/projects/llamanite.png" alt="LlamaNite" />
+				</div>
+
+				<div class="flex flex-col justify-around whitespace-nowrap py-1 pr-5 font-sans">
+					<h2 class="text-lg font-semibold italic">LlamaNite</h2>
+					<h3 class="text-xs">Back-End Developer</h3>
+					<p class="text-xs text-zinc-500">Jan 2021 - Aug 2022</p>
+				</div>
+
+				<div class="absolute -z-30 h-20 w-20 rounded-xl bg-violet-900 blur-md transition-all group-hover:bg-violet-800 group-hover:blur-lg" />
 			</a>
 		</div>
 	</div>
 
-	<div class="space-y-5 rounded-xl border-2 border-slate-700/40 p-5">
-		<h3 class="text-lg font-semibold italic">Skills</h3>
+	<div class="space-y-5 rounded-xl border-2 border-slate-700/40 p-5 xl:col-span-2">
+		<div class="flex items-center justify-between">
+			<h3 class="text-lg font-semibold italic">Skills</h3>
+			<a href="/" class="text-sm text-slate-300">View More</a>
+		</div>
 
 		<div class="flex flex-wrap gap-3">
 			{#each data.skills as skill}
