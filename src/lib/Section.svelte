49e2cd1f7de532f507ddb6e7b9c14909f612551f
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-09-27 19:58:51 +0330

    feat(lib): add Section componenent

---
+++ src/lib/Section.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let title: string;
+	export let href: string;
+</script>
+
+<div class="space-y-5 rounded-xl border-2 border-slate-700/40 p-5">
+	<div class="flex items-center justify-between">
+		<h3 class="text-lg font-semibold italic">{title}</h3>
+		<a {href} class="text-sm text-slate-300">View More</a>
+	</div>
+
+	<slot />
+</div>
